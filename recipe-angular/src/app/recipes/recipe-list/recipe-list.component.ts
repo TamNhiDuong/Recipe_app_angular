46dc1784cbb24c13716e3563e5b264a565b96495
TamNhiDuong <yanxinyee@gmail.com>
2020-02-10 17:23:07 +0200

    first sketch for recipe-list

--- recipe-angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'; 
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Testing description', 'https://images.unsplash.com/photo-1568899466260-b6d4e061856f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80')
+  ]
 
   constructor() { }
 
