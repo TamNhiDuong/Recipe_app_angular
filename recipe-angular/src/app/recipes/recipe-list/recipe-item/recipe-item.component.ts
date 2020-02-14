d210761433ef6ec07e53fbdf20c51ddab6aedde5
TamNhiDuong <yanxinyee@gmail.com>
2020-02-14 14:05:05 +0200

    use recipeModel instead of define again

--- recipe-angular/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-angular/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Recipe } from '../../recipe.model';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipeItem: {name: string, discription: string, imagePath: string}
+  @Input() recipeItem: Recipe;
 
   constructor() { }
 
