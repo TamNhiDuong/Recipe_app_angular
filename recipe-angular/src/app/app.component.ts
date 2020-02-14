c2c2ba0661b4192b5bb8a3a761303a4f5c161300
TamNhiDuong <yanxinyee@gmail.com>
2020-02-14 14:02:54 +0200

    passing data from parent to child

--- recipe-angular/src/app/app.component.ts
+++ recipe-angular/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-angular';
-  nav: string = '';
+  nav: string = 'recipe';
 
   selectedNav(userSelect: string) {
     if(userSelect === 'recipe') {
